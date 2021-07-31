c981443d2451727628312933d13d1e5c2a877cf6
Calum Andrew Morrell <calum@drulum.com>
2021-07-31 20:22:16 +0100

    preparing basic version for testing

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+# from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    def __str__(self):
+        if self.first_name:
+            return self.first_name
+        else:
+            return self.username
--- issues/models.py
+++ issues/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Report:
+
+    NATURE_CHOICES = [
+        (1, 'I would like to report a problem'),
+        (2, 'I would like to suggest a feature'),
+    ]
+
+    CATEGORY_CHOICES = [
+        (1, 'Consider the categories to use')
+    ]
+
+    SEVERITY_CHOICES = [
+        (1, 'Text'),
+        (2, 'Minor'),
+        (3, 'Major'),
+        (4, 'Crash'),
+    ]
+
+    PRIORITY_CHOICES = [
+        (1, 'Low'),
+        (2, 'Normal'),
+        (3, 'High'),
+        (4, 'Immediate'),
+    ]
+
+    REPRODUCIBILITY_CHOICES = [
+        (1, 'Always'),
+        (2, 'Sometimes'),
+        (3, 'Random'),
+        (4, 'Have not tried'),
+        (5, 'Unable to reproduce'),
+    ]
+
+    reporter = models.ForeignKey(get_user_model(),
+                                 on_delete=models.CASCADE)
+    # 'project' field once projects app designed
+    summary = models.CharField(max_length=200)
+    description = models.TextField()
+    nature = models.IntegerField()
+    category = models.IntegerField(blank=True, null=True)
+    severity = models.IntegerField(blank=True, null=True)
+    priority = models.IntegerField(blank=True, null=True)
+    reproducibility = models.IntegerField(blank=True, null=True)
+    # 'assigned_to' field once groups are in place
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
